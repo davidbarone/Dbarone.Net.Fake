bbba7510c0387885dd4e3a72286b40265a541487
davidbarone <davidbarone@live.com>
2024-04-02 21:10:42 +1100

    Added test

--- Dbarone.Net.Fake.Tests/Fake/Samplers/MarkovChain/MarkovChainTrainer.Tests.cs
+++ Dbarone.Net.Fake.Tests/Fake/Samplers/MarkovChain/MarkovChainTrainer.Tests.cs
@@ -122,6 +122,7 @@ public class MarkovChainTrainerTests
     [InlineData("When the going gets tough, the tough get going.", 1, MarkovChainLevel.Word, 9)]    // 6 words, 2 punctuation, and BOF marker = 9
     [InlineData("When the going gets tough, the tough get going.", 2, MarkovChainLevel.Word, 12)]    // BOF/BOF BOF/When When/the the/going going/gets gets/tough tough/, ,/the the/tough tough/get get/going going/. = 12
     [InlineData("When the going gets tough, the tough get going.", 1, MarkovChainLevel.Character, 11)]    // BOF W h e n t g o i s u  = 11
+    [InlineData("When the going gets tough, the tough get going.", 2, MarkovChainLevel.Character, 19)]    // BOF/BOF BOF/W Wh he en BOF/t th BOF/g go oi in ng ge et ts to ou ug gh = 19
     public void SimpleWordTrainers(string corpus, int order, MarkovChainLevel level, int expectedMatrixSize)
     {
         MarkovChainTrainerConfiguration configuration = new MarkovChainTrainerConfiguration
